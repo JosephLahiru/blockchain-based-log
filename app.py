da7e2ca7e06a69ec81fc452e333b424fece0d329
theAlphaGuardian <joseph.rasanjana1@gmail.com>
2023-09-13 15:53:25 +0000

    minor

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import requests
 from flask import Flask, request, jsonify, make_response, render_template
 
 app = Flask(__name__)
+
 
 def add_headers(output):
     response = make_response(jsonify(output))
